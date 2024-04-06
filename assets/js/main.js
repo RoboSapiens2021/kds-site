9c61017f06fc4dfb7452b41097c0d19d67de9f47
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2024-04-06 01:19:20 -0400

    carousel and video

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,6 +148,7 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(`.home__data`);
+sr.reveal(`.mySlides`);
 sr.reveal(`.home__img`, { delay: 500 });
 sr.reveal(`.home__social`, { delay: 600 });
 sr.reveal(`.about__img, .contact__box`, { origin: "left" });
@@ -155,3 +156,31 @@ sr.reveal(`.about__data, .contact__form`, { origin: "right" });
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
    interval: 100
 });
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+   showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+   showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("dot");
+   if (n > slides.length) { slideIndex = 1 }
+   if (n < 1) { slideIndex = slides.length }
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
+}
